5402e2ab52c3fc4b3fe9f9bce7abcf9a5cfa25dd
Karbofos <karbofos22@yandex.ru>
2022-09-20 20:14:17 +0300

    added HomeWork_1 files

---
+++ HomeWork_1/Controllers/MyWeatherController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherTest.Models;
+
+namespace WeatherTest.Controllers
+{
+    [Route("api/weather")]
+    [ApiController]
+    public class MyWeatherController : ControllerBase
+    {
+        private readonly WeatherForecastHolder _holder;
+
+        public MyWeatherController(WeatherForecastHolder holder)
+        {
+            _holder = holder;
+        }
+
+        [HttpPost("Add weather measurements")]
+        public IActionResult Add([FromQuery] DateTime date, [FromQuery] int temperatureC)
+        {
+            _holder.Add(date, temperatureC);
+            return Ok();
+        }
+        [HttpPut("Update weather measurements")]
+        public IActionResult Update([FromQuery] DateTime date, [FromQuery] int temperatureC)
+        {
+            _holder.Update(date, temperatureC);
+            return Ok();
+        }
+        [HttpDelete("Delete weather measurements by date")]
+        public IActionResult Delete([FromQuery] DateTime date)
+        {
+            _holder.Delete(date);
+            return Ok();
+        }
+        [HttpDelete("Fully delete weather record by date")]
+        public IActionResult FullyDeleteRecordByDate([FromQuery] DateTime date)
+        {
+            _holder.FullyDeleteRecordByDate(date);
+            return Ok();
+        }
+        [HttpGet("Get weather measurements by date")]
+        public IActionResult Get([FromQuery] DateTime dateFrom, [FromQuery] DateTime dateTo)
+        {
+            return Ok(_holder.Get(dateFrom, dateTo));
+        }
+    }
+}
---
+++ HomeWork_1/Models/WeatherForecastHolder.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherTest.Models
+{
+    public class WeatherForecastHolder
+    {
+        private List<WeatherForecast> _values;
+
+        public WeatherForecastHolder()
+        {
+            _values = new List<WeatherForecast>();
+        }
+
+        /// <summary>
+        /// Добавить новое измерение температуры
+        /// </summary>
+        /// <param name="date">Дата фиксации температуры</param>
+        /// <param name="temperatureC">Температура</param>
+        public void Add(DateTime date, int temperatureC)
+        {
+            _values.Add(new WeatherForecast() { Date = date, TemperatureC = temperatureC });
+        }
+        /// <summary>
+        /// Обновить существующее измерение температуры
+        /// </summary>
+        /// <param name="date">Дата фиксации температуры</param>
+        /// <param name="temperatureC">Температура</param>
+        public bool Update(DateTime date, int temperatureC)
+        {
+            foreach (var item in _values)
+            {
+                if(item.Date == date)
+                {
+                    item.TemperatureC = temperatureC;
+                    return true;
+                }
+            }
+            return false;
+        }
+        /// <summary>
+        /// Получить показатели температуры за временной период
+        /// </summary>
+        /// <param name="dateFrom">Начальная дата</param>
+        /// <param name="dateTo">Конечная дата</param>
+        /// <returns>Коллекция показателей температуры</returns>
+        public List<WeatherForecast> Get(DateTime dateFrom, DateTime dateTo)
+        {
+            return _values.FindAll(item => item.Date >= dateFrom && item.Date <= dateTo);
+        }
+        /// <summary>
+        /// Удалить показатель температуты на дату
+        /// </summary>
+        /// <param name="date">Дата фиксации показателя температуры</param>
+        /// <returns>Результат выполнения операции</returns>
+        public bool Delete(DateTime date)
+        {
+            foreach (var item in _values)
+            {
+                if (item.Date == date)
+                {
+                    item.TemperatureC = 0;
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool FullyDeleteRecordByDate(DateTime date)
+        {
+            foreach (var item in _values)
+            {
+                if (item.Date == date)
+                {
+                    _values.Remove(item);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}

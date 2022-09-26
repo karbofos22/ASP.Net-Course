791eb237f6ad6a80d93f2161be8ccd2e68f68ac1
Karbofos <karbofos22@yandex.ru>
2022-09-26 18:26:33 +0300

    added settings

--- MetricsApp/Metrics/MetricsAgent/Controllers/CpuMetricsController.cs
+++ MetricsApp/Metrics/MetricsAgent/Controllers/CpuMetricsController.cs
@@ -7,16 +7,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MetricsAgent.Controllers
 {
-    // TODO: Домашнее задание [Пункт 2]
-    // В проект агента сбора метрик добавьте контроллеры для сбора метрик, аналогичные
-    // менеджеру сбора метрик.Добавьте методы для получения метрик с агента, доступные по
-    //следующим путям
-    // a. api/metrics/cpu/from/{fromTime}/to/{toTime} [ВЫПОЛНИЛИ ВМЕСТЕ]
-    // b. api / metrics / dotnet / errors - count / from /{ fromTime}/ to /{ toTime} - готово
-    // c. api / metrics / network / from /{ fromTime}/ to /{ toTime} - готово
-    // d. api / metrics / hdd / left / from /{ fromTime}/ to /{ toTime} [ВЫПОЛНИЛИ ВМЕСТЕ]
-    // e. api / metrics / ram / available / from /{ fromTime}/ to /{ toTime} - готово
-
     [Route("api/metrics/CPU")]
     [ApiController]
     public class CpuMetricsController : ControllerBase
